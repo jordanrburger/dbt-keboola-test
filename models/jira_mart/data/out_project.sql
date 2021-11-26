857c18f8aacd94634c273a02ed127906e21bcde7
hrumichal <65334875+hrumichal@users.noreply.github.com>
2021-11-26 15:00:47 +0100

    models

---
+++ models/jira_mart/data/out_project.sql
@@ -0,0 +1,22 @@
+SELECT
+    "id" AS "project_id" -- PK
+    ,"key" AS "project_key"
+    ,"name" AS "project_name"
+    ,"project_category_name"
+    ,"archived" AS "archived"
+    ,CASE WHEN "project_category_name" = 'Keboola Internal' THEN '0011t00000GslojAAB' 
+          ELSE PARSE_JSON("description"):SFDC_ID::VARCHAR(255) 
+        END AS "sfdc_id"
+    ,CASE WHEN "project_category_name" = 'Keboola Internal' THEN '0' 
+          ELSE PARSE_JSON("description"):BUDGET::VARCHAR(255) 
+        END AS "budget"
+    ,CASE WHEN "project_category_name" = 'Keboola Internal' THEN '0' 
+          ELSE PARSE_JSON("description"):PRICE::VARCHAR(255) 
+        END AS "hour_rate"
+FROM {{ source('"WORKSPACE_7938092"', '"projects"') }}
+WHERE
+    "project_category_id" IN ('10001','10004') -- Project Implementation and Internal
+    AND
+    "project_id" NOT IN ('10028') -- not old CSAS project - rubbish data
+    AND
+    "archived" != 'True'
---
+++ models/jira_mart/snapshot_raw/out_worklogs.sql
@@ -0,0 +1,15 @@
+SELECT
+	  "id"
+    ,"issue_id"
+    ,"author_account_id"
+    ,"author_display_name"
+    ,"update_author_account_id"
+    ,"update_author_display_name"
+    ,"created"
+    ,"updated"
+    ,"started"
+    ,"time_spent"
+    ,"time_spent_seconds"
+    ,"comment"
+    ,CURRENT_DATE() AS "snapshot_date"
+FROM {{ source('"WORKSPACE_7938092"', '"worklogs"') }}
