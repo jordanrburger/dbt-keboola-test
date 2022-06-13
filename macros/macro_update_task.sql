cc3bb28c02870c841fabc2dea9c60de6a11ccdc8
jordanrburger <jordanrburger@gmail.com>
2022-06-13 08:24:55 -0400

    Update and rename macro_update_task.sql to enabled_vars.sql

--- macros/macro_update_task.sql
+++
@@ -1,8 +0,0 @@
-{% macro macro_update_task() %} 
-update {{ ref('out_task') }} set "task_type" = 'epic' 
-where "task_id" IN (SELECT DISTINCT("task_id") FROM {{ this }} );
-UPDATE {{ ref('out_task') }} t SET t."parent_id" = tl."task_id"
-FROM {{ this }} tl
-WHERE t."epic_name_link" = tl."task_key" AND
-      t."epic_name_link" IS NOT NULL AND t."parent_id" = '';
-{% endmacro %}
